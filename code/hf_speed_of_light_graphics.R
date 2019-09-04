62c17cfd4c5b2fad96490a99dcb5e98cb4ced7f8
akuyper <a-kuyper@northwestern.edu>
2019-09-04 05:30:07 -0500

    inset plot figure 1A/B

--- code/hf_speed_of_light_graphics.R
+++ code/hf_speed_of_light_graphics.R
@@ -12,6 +12,7 @@
 library(tidyverse)
 
 # Code for arranging plots
+library(cowplot)
 library(gridExtra)
 
 # Standard theme for graphics
@@ -154,6 +155,110 @@ ggplot(cdat) +
   stdtheme
 
 ggsave("./graphics/Figure 1A Single Pane.pdf", height = 10, width = 12)
+
+
+###---Combined plot for Figure 1A with inset 
+main_plot <- cdat %>% 
+  filter(year < 1960) %>% 
+  mutate(flag = year == lag(year),
+         flag = replace(flag, is.na(flag), FALSE),
+         year = replace(year, flag == TRUE, year + 0.5)) %>% 
+  ggplot() + 
+  geom_pointrange(aes(x = year, 
+                      y = estimate_adj, 
+                      ymin = ifelse(estimate_adj - probable_error < 299740,
+                                    299740,
+                                    estimate_adj - probable_error), 
+                      ymax = ifelse(estimate_adj + probable_error > 300000, 
+                                    300000,
+                                    estimate_adj + probable_error)),
+                  fatten = 0.75) + 
+  scale_y_continuous(name = "Measured speed of light (km/sec)",
+                     limits = c(299750, 300000),
+                     breaks = seq(299750, 300000, 25),
+                     expand = expansion(mult = 0.02)) + 
+  scale_x_continuous(name = "Year of experiment",
+                     limits = c(1870, 1960),
+                     breaks = seq(1870, 1960, 10),
+                     expand = expansion(mult = 0.02)) + 
+  coord_cartesian(clip = 'off') +
+  geom_hline(yintercept = rec_val, lty=2, color = "grey60") + 
+  annotate(geom = "text", size = 3.95,
+           x = 1878, y = rec_val - 2,
+           hjust = 0, vjust = 1,
+           label = c(paste(rec_val_year, "Recommended Value")),
+           family = "Times") + 
+  theme_classic() +
+  theme(text = element_text(family = "Times"),                            axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14), 
+        title = element_text(size = 16),
+        axis.title.x = element_text(hjust = 1),
+        axis.title.y = element_text(hjust = 1))
+
+# inset plot
+inset_plot <- cdat %>% 
+  filter(year >= 1945 & year < 1960) %>% 
+  mutate(flag = year == lag(year),
+         flag = replace(flag, is.na(flag), FALSE),
+         year = replace(year, flag == TRUE, year + 0.5)) %>% 
+ggplot() + 
+  geom_pointrange(aes(x = year, 
+                      y = estimate_adj, 
+                      ymin = ifelse(estimate_adj - probable_error < 299740,
+                                    299740,
+                                    estimate_adj - probable_error), 
+                      ymax = ifelse(estimate_adj + probable_error > 300000, 
+                                    300000,
+                                    estimate_adj + probable_error)),
+                  position = position_jitter(),
+                  fatten = 0.75) + 
+  scale_y_continuous(name = "Measured speed of light (km/sec)",
+                     limits = c(299787.5, 299796.5),
+                     breaks = seq(299788, 299796, 2),
+                     expand = expansion(mult = 0.02)) + 
+  scale_x_continuous(name = "Year of experiment",
+                     limits = c(1946.5, 1961),
+                     breaks = seq(1950, 1960, 5),
+                     expand = expansion(mult = 0.02)) + 
+  coord_cartesian(clip = 'off') +
+  geom_hline(yintercept = rec_val, lty=2, color = "grey60") + 
+  annotate(geom = "text", size = 3.5,
+           x = 1956, y = rec_val - 1.1,
+           hjust = 0, vjust = 1,
+           label = c(paste(rec_val_year, "Recommended Value")),
+           family = "Times") +
+  geom_segment(aes(x = 1957, y = rec_val-1, 
+                 xend = 1956.5, yend = rec_val - 0.1),
+             arrow = arrow(length = unit(0.02, "npc"), type = "closed" ),
+             size = 0.1, color = "grey40") +
+  ggtitle("Rescaled for estimates between 1945 and 1960") +
+  theme_classic() +
+  theme(text = element_text(family="Times"),
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14), 
+        title = element_text(size = 12),
+        axis.title.x = element_text(hjust = 1, size = 12),
+        axis.title.y = element_text(hjust = 1, size = 12),
+        plot.background = element_rect(colour = "black", 
+                                        size = 0.75))
+
+# Place the inset
+plot_with_inset <-
+  ggdraw() +
+  draw_plot(main_plot) +
+  draw_plot(inset_plot,
+            hjust = 1, vjust = 1,
+            x = 0.99, y = 0.99, 
+            width = 0.475, height = 0.475)
+  # draw_plot_label(
+  #   c("(A)", "(B)"),
+  #   c(0, 0.45),
+  #   c(1, 0.95),
+  #   size = 12,
+  #   family = "Times"
+  # )
+
+ggsave("./graphics/Figure 1A Single Pane with inset.pdf", plot_with_inset, height = 9, width = 12)
 
 
 ##-------------------------------------------------------------------------------##
